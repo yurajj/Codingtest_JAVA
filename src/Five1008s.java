ef9d1cd0be2a9297001996761deca9f93e5acd8f
yurajj <yul_j@naver.com>
2023-05-24 15:42:46 +0900

    first commit

---
+++ src/Five1008s.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Five1008s {
+
+    public static void main(String[] args) {
+
+
+        // 1008번 https://www.acmicpc.net/problem/1008
+        // 문제 : 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+
+
+
+        Scanner D = new Scanner(System.in);
+
+            Double A = D.nextDouble();
+            Double B = D.nextDouble();
+
+        System.out.println(A/B);
+
+        D.close();
+
+    }
+    
+}
---
+++ src/One2557s.java
@@ -0,0 +1,13 @@
+public class One2557s {
+
+    public static void main(String[] args) {
+
+        // 2557번 https://www.acmicpc.net/problem/2557
+        // 문제 : Hello World!를 출력하시오.
+        
+        System.out.println("Hello World!");   
+          
+        // return 0;
+    }
+    
+}
