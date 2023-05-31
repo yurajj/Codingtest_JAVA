8d08f3a2cfafc8391d8d9a5fe0fb0643f86287a2
yurajj <yul_j@naver.com>
2023-05-31 15:39:46 +0900

    java codingtest

---
+++ src/A13Q10172s.java
@@ -0,0 +1,17 @@
+public class A13Q10172s {
+
+    
+    // 10172번 https://www.acmicpc.net/problem/10172
+    // 문제 : 아래 예제와 같이 개를 출력하시오.
+
+
+    public static void main(String[] args) {
+    
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
